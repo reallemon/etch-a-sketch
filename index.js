5f84006340509fad752c7e8d93c37146ff438a15
Spencer Penrod <spencerpenrod@gmail.com>
2022-12-18 20:54:14 -0700

    Basic generation for arbitrary canvas sizes and basic styling

---
+++ index.js
@@ -0,0 +1,25 @@
+const canvas = document.querySelector('#canvas');
+const canvasHeight = 500;
+
+function resetCanvas() {
+  while (canvas.firstChild) {
+    canvas.removeChild(canvas.firstChild);
+  }
+}
+
+function createPixels(squareSize) {
+  const pixelPercentage = Math.floor(100 / squareSize);
+  const pixelHeight = Math.floor(canvasHeight / squareSize);
+  resetCanvas();
+
+  for (let i = 0; i < squareSize * squareSize; i++) {
+    const pixel = document.createElement('div');
+    pixel.classList.add('canvas-pixel');
+    pixel.style.flex = `0 0 ${pixelPercentage}%`;
+    pixel.style.height = `${pixelHeight}px`;
+
+    canvas.appendChild(pixel);
+  }
+}
+
+createPixels(16);
